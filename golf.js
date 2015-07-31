6cacccca746b2bbb37cbe4f57560bf5a5d458943
xem <maxime.euziere@gmail.com>
2015-07-31 21:26:56 +0200

    258 by subzey

--- golf.js
+++ golf.js
@@ -1,1 +1,1 @@
-b=[c=""];for(d=e=f=g=i=0;500>i;i++)c+="  "+"⡆⡅⡃⠇"[b[i]=4*Math.random()|0];onkeyup=a=>d+=a.keyCode-39;setInterval('d<0&&(d=0);d>3&&(d=3);3<e&&!(e%30)?b[g++]==d?(h="⡇",f++):alert(f):h="⠁⠂⠄⡀"[d];history.replaceState(0,0,"#"+(h+c.slice(e++/10)))',30)
+b=[c=""];for(d=e=f=g=i=0;500>i;i++)c+="  "+"⡆⡅⡃⠇"[b[i]=4*Math.random()|0];onkeyup=a=>d+=a.keyCode-39;setInterval('d=0>d?0:3<d?3:d;3<e&&!(e%30)?b[g++]==d?(h="⡇",f++):alert(f):h="⠁⠂⠄⡀"[d];history.replaceState(0,0,"#"+(h+c.slice(e++/10)))',30)
