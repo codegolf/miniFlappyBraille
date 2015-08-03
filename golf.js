0fb1948867da8e578e6a686bf1b7437ce231e9b5
xem <maxime.euziere@gmail.com>
2015-08-03 19:31:16 +0200

    204

--- golf.js
+++ golf.js
@@ -1,1 +1,1 @@
-g=b=[c=""];for(d=e=f=i=0;g;onkeyup=a=>d+=a.which-39)c+="  "+"⡆⡅⡃⠇"[b[i++]=(g=7*++g%97)%4];setInterval('3<e&&!(e%27)?i&&b[g++]==d?f++:i=0:h="⠈⠐⠠⢀"[d];location.hash=i?[h]+c.slice(e++/9):f',27)
+g=b=[c=""];for(d=e=f=i=0;g;onkeyup=a=>d+=a.which-39)c+="  "+"⡆⡅⡃⠇"[b[i++]=(g=7*++g%97)%4];setInterval('3<e&&!(e%27)?i&&b[g++]==d?f++:i=0:i;location.hash=i?["⠈⠐⠠⢀"[d]]+c.slice(e++/9):f',27)
