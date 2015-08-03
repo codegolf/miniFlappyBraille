a2ae643b6b3467f5ad4e85fed51153ad1d0e9fd9
xem <maxime.euziere@gmail.com>
2015-08-03 19:29:37 +0200

    206 by aemkei

--- golf.js
+++ golf.js
@@ -1,1 +1,1 @@
-b=[c=""];for(d=e=f=g=i=0;99>i;)c+="  "+"⡆⡅⡃⠇"[b[i++]=4*Math.random()|0];onkeyup=a=>d+=a.which-39;setInterval('3<e&&!(e%27)?i&&b[g++]==d?f++:i=0:h="⠈⠐⠠⢀"[d];location.hash=i?[h]+c.slice(e++/9):f',27)
+g=b=[c=""];for(d=e=f=i=0;g;onkeyup=a=>d+=a.which-39)c+="  "+"⡆⡅⡃⠇"[b[i++]=(g=7*++g%97)%4];setInterval('3<e&&!(e%27)?i&&b[g++]==d?f++:i=0:h="⠈⠐⠠⢀"[d];location.hash=i?[h]+c.slice(e++/9):f',27)
